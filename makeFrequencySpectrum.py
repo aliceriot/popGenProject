252c1e8887b3fa1b7622e234b5b038957152ba6f
Benjamin Pote <ben.pote@gmail.com>
2014-11-05 11:22:58 -0500

    initial commit

---
+++ makeFrequencySpectrum.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python2
+
+#note that the #! line is for my (ben's) machine, you may need to change it
+#if you're on mac OS or windows
+
+import dadi
+
+
---
+++ makeSnpFile.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+
+#this one is using python3 because it doesn't need anything from dadi, so
+#we're free to use the superior python
