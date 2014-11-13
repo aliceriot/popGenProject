aacfca7c5ff53bda62521a751011923e3254e34b
Benjamin Pote <ben.pote@gmail.com>
2014-11-13 10:02:23 -0500

    Initial plot of baby AFS

--- makeFrequencySpectrum.py
+++ makeFrequencySpectrum.py
@@ -5,4 +5,15 @@
 
 import dadi
 
+#we make a dictionary of the snp data
+dd = dadi.Misc.make_data_dict('hunsteinAsnpFile.txt')
 
+#now we can make a spectrum with this data
+fs = dadi.Spectrum.from_data_dict(dd, ['1','2','3'],[20,20,20])
+
+#make one we can plot
+newfs = dadi.Spectrum.from_data_dict(dd, ['1','2'],[20,20])
+
+import pylab
+dadi.Plotting.plot_single_2d_sfs(newfs, vmin=0.1)
+pylab.show()
