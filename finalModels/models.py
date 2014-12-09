3405e32afe51d17018c04bccb09a575d27c02b09
Benjamin Pote <ben.pote@gmail.com>
2014-12-09 12:13:22 -0500

    Model 1 added (no migration)

---
+++ finalModels/models.py
@@ -0,0 +1,18 @@
+import numpy
+import dadi
+
+def modelOne(params, ns, pts):
+    s,nu1,nu2,T = params
+
+    xx = dadi.Numerics.default_grid(pts)
+
+    phi = dadi.PhiManip.phi_1D(xx)
+    phi = dadi.PhiManip.phi_1D_to_2D(xx, phi)
+
+    nu1_func = lambda t: s * (nu1/s)**(t/T)
+    nu2_func = lambda t: (1-s) * (nu2/(1-s)) ** (t/T)
+    phi = dadi.Integration.two_pops(phi, xx, T, nu1_func, nu2_func,
+            m12 = 0, m21 = 0)
+
+    fs = dadi.Spectrum.from_phi(phi,ns,(xx,xx))
+    return fs
