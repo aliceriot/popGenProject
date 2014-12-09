94272f237a4fbc5e4c6427b495b39a989f957f2d
Benjamin Pote <ben.pote@gmail.com>
2014-12-09 12:18:01 -0500

    Added model 2

--- finalModels/models.py
+++ finalModels/models.py
@@ -16,3 +16,22 @@ def modelOne(params, ns, pts):
 
     fs = dadi.Spectrum.from_phi(phi,ns,(xx,xx))
     return fs
+
+
+def modelTwo(params, ns, pts):
+    s,nu1,nu2,T,m12 = params
+
+    xx = dadi.Numerics.default_grid(pts)
+
+    phi = dadi.PhiManip.phi_1D(xx)
+    phi = dadi.PhiManip.phi_1D_to_2D(xx, phi)
+
+    nu1_func = lambda t: s * (nu1/s)**(t/T)
+    nu2_func = lambda t: (1-s) * (nu2/(1-s)) ** (t/T)
+    phi = dadi.Integration.two_pops(phi, xx, T, nu1_func, nu2_func,
+            m12 = m12, m21 = 0)
+
+    fs = dadi.Spectrum.from_phi(phi,ns,(xx,xx))
+    return fs
+
+
