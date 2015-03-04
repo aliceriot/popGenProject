259cce6e64f943c70827744943f504da3c52f30b
Benjamin Pote <ben.pote@gmail.com>
2015-03-04 14:54:55 -0500

    Added a 5th model

--- finalModels/models.py
+++ finalModels/models.py
@@ -34,6 +34,26 @@ def modelTwo(params, ns, pts):
     fs = dadi.Spectrum.from_phi(phi,ns,(xx,xx))
     return fs
 
+def modelFive(params, ns, pts):
+    """
+    this one is actually most similar to model 2!
+    """
+    s,nu1,nu2,T,m21 = params
+
+    xx = dadi.Numerics.default_grid(pts)
+
+    phi = dadi.PhiManip.phi_1D(xx)
+    phi = dadi.PhiManip.phi_1D_to_2D(xx, phi)
+
+    nu1_func = lambda t: s * (nu1/s)**(t/T)
+    nu2_func = lambda t: (1-s) * (nu2/(1-s)) ** (t/T)
+    phi = dadi.Integration.two_pops(phi, xx, T, nu1_func, nu2_func,
+            m12 = 0, m21 = m21)
+
+    fs = dadi.Spectrum.from_phi(phi,ns,(xx,xx))
+    return fs
+
+
 
 def modelThree(params, ns, pts):
     s,nu1,nu2,T,m = params
